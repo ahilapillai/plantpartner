e5030b751ebebadfa062898d8cfa8b2b40b02cb0
ahilapillai <contact.hues26@gmail.com>
2026-04-29 12:39:46 +0530

    fix: show vine on mobile at 45vh with slight opacity

--- components/Vine.tsx
+++ components/Vine.tsx
@@ -6,13 +6,13 @@ export default function Vine() {
   return (
     <div
       aria-hidden="true"
-      className="pointer-events-none select-none fixed top-0 right-0 h-screen overflow-hidden z-0 hidden md:block"
+      className="pointer-events-none select-none fixed top-0 right-0 overflow-hidden z-0"
     >
       {/* eslint-disable-next-line @next/next/no-img-element */}
       <img
         src="/images/hero-bg-img.png"
         alt=""
-        className="h-full w-auto object-contain"
+        className="h-[45vh] md:h-screen w-auto object-contain opacity-80 md:opacity-100"
       />
     </div>
   );
